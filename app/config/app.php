76f5cb1d69fdb031d804c75ffa7206420db7cd67
perials <sanket3737@gmail.com>
2019-06-30 23:39:06 +0530

    Updated app config

--- app/config/app.php
+++ app/config/app.php
@@ -5,7 +5,11 @@ return [
     'debug' => true,
 
 	//set this to boolean true to output executed database queries
-	'db_profiler' => false
+	'db_profiler' => false,
+
+	'swoole_server' => false,
+	// set base_url if using swoole_server
+	'base_url' => 'http://localhost:3000'
     
 ];
 
